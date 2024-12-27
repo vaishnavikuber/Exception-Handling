04cd03fb984dbf663122881e0760999850ecba82
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-27 17:16:47 +0530

    exception example

---
+++ ExceptionHandling/ZeroNotAllowedException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandling
+{
+    internal class ZeroNotAllowedException : Exception
+    {
+
+        public ZeroNotAllowedException(string message) : base(message) { }
+
+        public override string ToString()
+        {
+            return $"{Message}";
+        }
+
+    }
+}
